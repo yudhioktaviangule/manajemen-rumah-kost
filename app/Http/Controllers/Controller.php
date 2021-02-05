1f30bcea5debaecf062ad0206913b77dde9d4c13
AlimJr123 <aliembakri@gmail.com>
2021-02-05 21:27:14 +0700

    p

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    private $exelso="";
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
