5d8e9a879e0cb33b63f4a5cf06c14eb877d5a0f9
Yudhi Oktavian <yudhioktavian@icloud.com>
2021-02-11 20:32:42 +0800

    buat controller kamar dan web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PenyewaController;
+use App\Http\Controllers\KamarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +13,5 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 //Route::get('/penyewa', [App\Http\Controllers\PenyewaController::class, 'index'])->penyewa('penyewa');
 
+Route::resource('kamar',KamarController::class);
 Route::resource('penyewa',PenyewaController::class);
