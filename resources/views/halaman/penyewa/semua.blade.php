5efe92e011a04a8036ba61eb0f8c9cbed117f475
Yudhi Oktavian <yudhioktavian@icloud.com>
2021-02-12 22:30:16 +0800

    Menu OK

--- resources/views/halaman/penyewa/semua.blade.php
+++ resources/views/halaman/penyewa/semua.blade.php
@@ -4,8 +4,12 @@
 @section('content')
 <div class="box">
   <div class="box-header with-border">
-    <h3 class="box-title">Daftar Kamar</h3>
-    
+    <h3 class="box-title">Daftar Penyewa</h3>
+    <div class="box-tools pull-right">
+      <a href="{{ route('penyewa.create') }}" class="btn btn-primary btn-sm">
+          Register Penyewa
+      </a>
+    </div>
     <!-- /.box-tools -->
   </div>
   <!-- /.box-header -->
@@ -14,13 +18,25 @@
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>Nomor Kamar</th>
-                    <th>Harga</th>
-                    <th>Fasilitas</th>
+                    <th>Nama </th>
+                    <th>No.Hp</th>
+                    <th>Jenis Kelamin</th>
                     <th>&nbsp;</th>
                 </tr>
+               
             </thead>
-            <tbody></tbody>
+            <tbody>
+            @foreach($penyewas as $kunci => $value)
+              <tr>
+                <td>{{ $value->name}}</td>
+                <td>{{ $value->hp}}</td>
+                <td>{{ $value->jenis_kelamin}}</td>
+                <td>
+                    
+                </td>
+              </tr> 
+            @endforeach
+            </tbody>
         </table>
     </div>
   </div>
