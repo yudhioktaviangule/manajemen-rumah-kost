2e23903d6daabf11b87a936bc883a57477a2a8be
Yudhi Oktavian <yudhioktavian@icloud.com>
2021-02-15 19:49:37 +0800

    ok

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -1,41 +1,40 @@
 <li class="active"><a href="user"><i class="fa fa-link"></i> <span>User</span></a></li>
-       
-        <li class="treeview">
-          <a href="#">
+  <li class='treeview'>
+    <a href="penyewa">
+      <i class="fa fa-link"></i>
+      <span>Penyewa</span>
+      <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+      </span>
+    </a>
+    <ul class="treeview-menu">
+        <li>
+          <a href="{{ route('penyewa.index') }}">
               <i class="fa fa-link"></i> 
-              <span>Kamar</span>
-              <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-              </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Master Kamar</a></li>
-            <li><a href="{{route('fasilitas.index')}}">Fasilitas</a></li>
-            <li><a href="#">Perawatan </a></li>
-          </ul>
-        </li>
-        <li><a href="penyewa"><i class="fa fa-link"></i> <span>Aset</span></a></li>
-        <li class='treeview'>
-            <a href="penyewa">
-              <i class="fa fa-link"></i>
               <span>Penyewa</span>
-              <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-              </span>
-            </a>
-            <ul class="treeview-menu">
-                <li>
-                  <a href="{{ route('penyewa.index') }}">
-                      <i class="fa fa-link"></i> 
-                      <span>Penyewa</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="{{ route('kamar_sewa.index') }}">
-                      <i class="fa fa-link"></i> 
-                      <span>Data Kamar Penyewa</span>
-                  </a>
-                </li>
-             
-          </ul>
+          </a>
         </li>
+        <li>
+          <a href="{{ route('kamar_sewa.index') }}">
+              <i class="fa fa-link"></i> 
+              <span>Data Kamar Penyewa</span>
+          </a>
+        </li>
+  </ul>
+</li>
+<li class="treeview">
+  <a href="#">
+      <i class="fa fa-link"></i> 
+      <span>Kamar</span>
+      <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+      </span>
+  </a>
+  <ul class="treeview-menu">
+    <li><a href="#">Master Kamar</a></li>
+    <li><a href="{{route('fasilitas.index')}}">Fasilitas</a></li>
+    <li><a href="#">Perawatan </a></li>
+  </ul>
+</li>
+<li><a href="penyewa"><i class="fa fa-link"></i> <span>Aset</span></a></li>
+        
