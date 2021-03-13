d61eedee94c6f02e5548ba600ea96a98d5edacfe
yudhioktaviangule <yudhioktavian@icloud.com>
2021-03-13 11:24:39 +0800

    ubah menu penyewa

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -7,8 +7,6 @@
   <li class=""><a href="{{route('m_aset.index')}}"><i class="fa fa-circle-o"></i> <span>Aset</span></a></li>
   <li class=""><a href="{{ route('kamar.index') }}"><i class="fa fa-bed"></i> <span>Kamar</span></a></li>
   <li class=""><a href="{{ route('pengeluaran.index') }}"><i class="fa fa-credit-card"></i> <span>Pengeluaran</span></a></li>
-@else
-  <li class=""><a href="#"><i class="fa fa-credit-card"></i> <span>Pembayaran</span></a></li>
 @endif
 @if(Auth::user()->level==='admin')
   <li class=""><a href="{{ route('laporan.index') }}"><i class="fa fa-file-o"></i> <span>Laporan</span></a></li>
