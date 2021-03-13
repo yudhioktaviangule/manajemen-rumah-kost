c0e29cc9076aed38c97dc29b9be84db27a365e76
yudhioktaviangule <yudhioktavian@icloud.com>
2021-03-13 09:46:12 +0800

    fix bug aset kamar

--- app/Http/Controllers/Web/FasilitasController.php
+++ app/Http/Controllers/Web/FasilitasController.php
@@ -43,6 +43,7 @@ class FasilitasController extends Controller{
         $post = $request->only('keterangan','kamar_id','aset_id');
         $post['kondisi'] = $request->status;
         $fasilitas = new Fasilitas();
+        dd($post);
         $fasilitas->fill($post);
         $fasilitas->save();
         return redirect(route('fasilitas.kamar.terpilih',['kamar_id'=>$post['kamar_id']]));
